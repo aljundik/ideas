d280b28164bfe04585dca81b09a18ee375f8c108
aljundik <kaledj007@gmail.com>
2016-12-28 09:41:38 +0200

    Add idea endpoints

--- api/controllers/idea.controller.js
+++ api/controllers/idea.controller.js
@@ -31,3 +31,125 @@ module.exports.addIdea = function(req,res){
 		});
 
 };
+
+
+module.exports.getIdea = function(req,res){
+	console.log("we are inside getIdea")
+	Idea
+		.find()
+		.exec(function(err,idea){ 
+
+			if(err) {
+				console.log("Error finding idea");
+				res
+				.status(500)
+				.json(err);
+			} else{
+				console.log("found idea", idea.length);
+				res
+				.json(idea);
+			}
+		});
+
+
+};
+
+module.exports.getOneIdea = function(req,res){
+	var ideaId = req.params.ideaId;// req parameters handles the url partamters 
+
+	Idea
+	.findById(ideaId)
+	.exec (function(err,doc){
+		var response = {
+			status : 200,
+			message : doc
+		};
+		if(err) {
+			console.log("Error finding Idea");
+			response.status = 500;
+			response.message = err;
+		} else if (!doc){
+			response.status = 404;
+				response.message = {
+					"message" : "Idea ID not found"
+				};
+		}
+		res
+		.status(response.status)
+		.json(response.message);
+		
+	});
+	console.log("get ideaId", ideaId);
+};
+
+module.exports.editIdea =function(req,res){
+	var ideaId = req.params.ideaId;// req parameters handles the url partamters 
+
+	Idea
+	.findById(ideaId)
+	.exec (function(err,doc){
+		var response = {
+			status : 200,
+			message : doc
+		};
+		if(err) {
+			console.log("Error finding organize");
+			response.status = 500;
+			response.message = err;
+		} else if (!doc){
+			response.status = 404;
+				response.message = {
+					"message" : "Idea ID not found"
+				};
+		}
+		if (response.status !==200){
+			res
+				.status(response.status)
+				.json(response.message);
+			} else {
+				doc.ideaTitle = req.body.ideaTitle;
+				doc.ideaCategory = req.body.ideaCategory;
+				doc.ideaDescription = req.body.ideaDescription;
+
+				doc.save(function(err,ideaUpdated){
+					if (err) {
+						res
+							.status(500)
+							.json(err);
+					}else{
+						res
+							.status(204)
+							.json(response.message);
+					}
+				});
+			}
+
+
+		});	
+
+
+};
+
+module.exports.deleteIdea = function(req,res){
+	var ideaId = req.params.ideaId;// req parameters handles the url partamters
+
+	Idea
+		.findByIdAndRemove(ideaId)
+		.exec(function(err,idea){
+
+			if (err) {
+				res
+					.status(404)
+					.json(err);
+			} else{
+				console.log("idea deleted , id: " , ideaId);
+				res
+					.status(204)
+					.json();
+			}
+
+		});
+
+
+
+};
--- api/routes/index.js
+++ api/routes/index.js
@@ -7,7 +7,14 @@ var ctrlIdea = require('../controllers/idea.controller.js');
 
 router 
 	.route('/idea')
-	.post(ctrlIdea.addIdea);
+	.post(ctrlIdea.addIdea)
+	.get(ctrlIdea.getIdea);
+
+router
+	.route('/idea/:ideaId')
+	.get(ctrlIdea.getOneIdea)
+	.put(ctrlIdea.editIdea)
+	.delete(ctrlIdea.deleteIdea);
 
 
 module.exports = router;
